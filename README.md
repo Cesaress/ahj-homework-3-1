9f83a2ae3cc90e704238c2a894a6a18e7079bcab
Андрей Богданов <cesaress@yandex.ru>
2023-06-24 23:25:33 +0300

    hw 3-1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ahj-homework-3-1
+
+[![Build status](https://ci.appveyor.com/api/projects/status/0qwd5ktbrbfolpj8?svg=true)](https://ci.appveyor.com/project/Cesaress/ahj-homework-3-1)
+
+(https://cesaress.github.io/ahj-homework-3-1/)
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import RenderModal from './RenderModal';
+
+import background from '../img/cell.jpg';
+
+document.querySelector('body').style.backgroundImage = `url(${background})`;
+
+const container = document.querySelector('.app-container');
+const render = new RenderModal(container);
+
+render.enterUser();
